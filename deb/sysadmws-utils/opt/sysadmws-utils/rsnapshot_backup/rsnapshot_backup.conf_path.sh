71ea07e2c5dde73629ce6eaded90f709dcd2c960
Eugene Antonov <jetman@sysadm.ws>
2017-07-26 19:55:01 +0300

    config helper path fixed --- bug

--- deb/sysadmws-utils/opt/sysadmws-utils/rsnapshot_backup/rsnapshot_backup.conf_path.sh
+++ deb/sysadmws-utils/opt/sysadmws-utils/rsnapshot_backup/rsnapshot_backup.conf_path.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export HN=`hostname`
 export XPATH=$1
-echo "---			---			$XPATH		---" >> /opt/sysadmws-utils/rsnapshot_backup/rsnapshot_backup.conf
+echo "---			FS_RSYNC_SSH			$XPATH		---" >> /opt/sysadmws-utils/rsnapshot_backup/rsnapshot_backup.conf
 cat /opt/sysadmws-utils/salt/grains.path.template | sed "s/HOSTNAME/$HN/g" | sed "s/PATH/$XPATH/g" >> /etc/salt/grains
 /opt/sysadmws-utils/rsnapshot_backup/rsnapshot_backup.sh sync
