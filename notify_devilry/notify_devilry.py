b46ba84cf1bd52e040711bac4d69786117898743
Eugene Antonov <jetman@sysadm.ws>
2018-01-05 17:08:09 +0200

    minor fixes

--- notify_devilry/notify_devilry.py
+++ notify_devilry/notify_devilry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-from __future__ import (absolute_import, division, print_function)
 import os
 import shutil
 import sys
@@ -8,117 +7,122 @@ import datetime
 import yaml
 import urllib
 import urllib2
+import logging
 from collections import OrderedDict
 try:
     import json
 except ImportError:
     import simplejson as json
-from jinja2 import Environment, FileSystemLoader, Template
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 
 # Constants
+WORK_DIR = "/opt/sysadmws-utils/notify_devilry"
 CONFIG_FILE = "notify_devilry.yaml.jinja"
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Log methods
-def log_notice(msg):
-    now = datetime.datetime.now()
-    print(now.strftime("%F %T ") + "NOTICE: " + msg, file = sys.stdout)
-def log_error(msg):
-    now = datetime.datetime.now()
-    print(now.strftime("%F %T ") + "ERROR: " + msg, file = sys.stderr)
+# Set logging format and show up to debug
+logging.basicConfig(format='{} %(levelname)s: %(message)s'.format(datetime.datetime.now().strftime("%F %T")),level=logging.DEBUG)
+
+# Custom Exceptions
+class LoadJsonError(Exception):
+    pass
+class CheckJsonKeyError(Exception):
+    pass
+#class LoadYamlConfigError(Exception):
+#    pass
+
+# Load JSON from stdin
+def load_json(f):
+    try:
+        message = json.load(f, object_pairs_hook=OrderedDict)
+    except:
+        try:
+            message = json.load(f)
+	except:
+            try:
+                stdin_data = f.read()
+                message = json.loads(stdin_data)
+            except:
+                raise LoadJsonError("Reading JSON message from file '{}' failed".format(f))
+    return message
+
+# Check needed key in dict
+def check_json_key(key, msg):
+    if not key in msg:
+        raise CheckJsonKeyError("No '{}' key in JSON message '{}'".format(key, msg))
+
+# Load YAML config
+def load_yaml_config(d, f):
+    # Get in the env
+    j2_env = Environment(loader=FileSystemLoader(d), trim_blocks=True)
+    # our config is a template
+    try:
+        template = j2_env.get_template(f)
+    except TemplateNotFound:
+        # It is ok if no config file, display no error, return None
+        return None
+    # Set vars inside config file and render
+    current_date = datetime.datetime.now().strftime("%Y%m%d")
+    current_time = datetime.datetime.now().strftime("%H%M%S")
+    logging.info("current_date = {}".format(current_date))
+    logging.info("current_time = {}".format(current_time))
+    config_dict = yaml.load(template.render(
+        current_date = int(current_date),
+        current_time = int(current_time)
+    ))
+    return config_dict
 
 # Send methods
+# Telegram
 def send_telegram(token, chat_id, msg):
-    log_notice("Sending to TG API result: " + urllib2.urlopen("https://api.telegram.org/bot" + token + "/sendMessage", urllib.urlencode({"parse_mode": "HTML", "chat_id": chat_id, "text": msg })).read())
+    # Format message as text
+    message_as_text = ""
+    for m_key, m_val in msg.items():
+        message_as_text = "{}{}: <b>{}</b>\n".format(message_as_text, m_key, m_val)
+    url_url = "https://api.telegram.org/bot{}/sendMessage".format(token)
+    url_data = urllib.urlencode({"parse_mode": "HTML", "chat_id": chat_id, "text": message_as_text })
+    url_obj = urllib2.urlopen(url_url, url_data)
+    url_result = url_obj.read()
+    logging.info("Sending to TG API result: {}".format(url_result))
 
 if __name__ == "__main__":
 
-    try:
-        log_notice("Starting notify_devilry")
+    # Load YAML config
+    config_dict = load_yaml_config(WORK_DIR, CONFIG_FILE)
+    # If config is None just exit silently
+    if config_dict is None:
+        sys.exit(1)
 
-        # Set default exit code
-        exit_code = 0
+    # Read json message from stdin
+    message = load_json(sys.stdin)
+    # Check needed keys
+    check_json_key("host", message)
+    check_json_key("from", message)
 
-        # Read json message from stdin
-        try:
-            try:
-                message = json.load(sys.stdin, object_pairs_hook=OrderedDict)
-            except Exception:
+    # Check if enabled in config
+    if not config_dict['notify_devilry']['enabled']:
+        logging.warning("notify_devilry not enabled in config, exiting")
+        sys.exit(1)
+
+    logging.info("Starting notify_devilry")
+
+    # Iterate over notify dict and send message for each
+    for notify_item in config_dict['notify_devilry']['notify']:
+        # In case notify item has a match filter
+        should_continue = False
+        if 'match' in notify_item:
+            # Check and skip item if not matched
+            for match_item_key, match_item_val in notify_item['match'].items():
+                if message[match_item_key] != match_item_val:
+                    should_continue = True
+        if should_continue:
+            continue
+        # Should send telegram, iterate over telegram aliases
+        if 'telegram' in notify_item:
+            for telegram_item in notify_item['telegram']:
+                # Even if sending failed, catch and show exception and try send next methods
                 try:
-                    message = json.load(sys.stdin)
-                except Exception:
-                    stdin_data = sys.stdin.read()
-                    message = json.loads(stdin_data)
-            if not 'host' in message:
-                raise Exception("No 'host' key in message dict")
-            if not 'from' in message:
-                raise Exception("No 'from' key in message dict")
-            # Format message as text
-            message_as_text = ""
-            for m_key, m_val in message.items():
-                message_as_text = message_as_text + m_key + ": " + "<b>" + m_val + "</b>" + "\n"
-        except Exception as e:
-            log_error(e.message)
-            exit_code = 1
-            raise Exception("Reading JSON message from stdin failed, exiting")
+                    send_telegram(config_dict['notify_devilry']['telegram'][telegram_item]['token'], config_dict['notify_devilry']['telegram'][telegram_item]['chat_id'], message)
+                except Exception as e:
+                    logging.exception(e)
 
-        try:
-            j2_env = Environment(loader = FileSystemLoader(THIS_DIR),trim_blocks = True)
-        except Exception as e:
-            log_error(e.message)
-
-        # Get config file
-        try:
-            template = j2_env.get_template(CONFIG_FILE)
-        except Exception as e:
-            log_error(e.message)
-
-        # Set vars inside config file and render
-        try:
-            current_date = datetime.datetime.now().strftime("%Y%m%d")
-            current_time = datetime.datetime.now().strftime("%H%M%S")
-            log_notice("current_date = " + current_date)
-            log_notice("current_time = " + current_time)
-            config_dict = yaml.load(template.render(
-                current_date = int(current_date),
-                current_time = int(current_time)
-            ))
-        except Exception as e:
-            log_error(e.message)
-
-        # Check if enabled in config
-        try:
-            if not config_dict['notify_devilry']['enabled']:
-                raise Exception("notify_devilry not enabled in config, exiting")
-        except:
-            raise Exception("notify_devilry not enabled in config, exiting")
-
-        # Iterate over notify dict and send message for each
-        for notify_item in config_dict['notify_devilry']['notify']:
-            try:
-                # In case notify item has a match filter
-                should_continue = False
-                if 'match' in notify_item:
-                    # Check and skip item if not matched
-                    for match_item_key, match_item_val in notify_item['match'].items():
-                        if message[match_item_key] != match_item_val:
-                            should_continue = True
-                if should_continue:
-                    continue
-                # Should send telegram, iterate over telegram aliases
-                if 'telegram' in notify_item:
-                    for telegram_item in notify_item['telegram']:
-                        try:
-                            send_telegram(config_dict['notify_devilry']['telegram'][telegram_item]['token'], config_dict['notify_devilry']['telegram'][telegram_item]['chat_id'], message_as_text)
-                        except Exception as e:
-                            log_error(e.message)
-            except Exception as e:
-                log_error(e.message)
-
-    except Exception as e:
-        log_error(e.message)
-
-    finally:
-	# Clean tmps here, if any
-        log_notice("Finished notify_devilry")
-        sys.exit(exit_code)
+    logging.info("Finished notify_devilry")
