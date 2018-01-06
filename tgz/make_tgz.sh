9a8ee14d80fe1f668fd26baa0c2f215c01cf9c36
Eugene Antonov <jetman@sysadm.ws>
2018-01-07 01:27:41 +0200

    version added to tgz

--- tgz/make_tgz.sh
+++ tgz/make_tgz.sh
@@ -5,6 +5,7 @@ if [ `dirname $0` != "." ]; then
 fi
 rm -rf sysadmws-utils
 cp -R ../deb/sysadmws-utils .
+cat sysadmws-utils/DEBIAN/control | grep Version > sysadmws-utils/opt/sysadmws-utils/VERSION
 rm -rf sysadmws-utils/DEBIAN
 sudo chown -R root:root sysadmws-utils
 sudo tar zcf sysadmws-utils.tar.gz -C sysadmws-utils .
