b256f762a70d12e273831a980e91d92c3830b252
Eugene Antonov <jetman@sysadm.ws>
2018-08-10 14:17:15 +0300

    rsnapshot_backup_sync_monthly_weekly_daily_check_backup exit code agg

--- rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh
+++ rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+GRAND_EXIT=0
+
 /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh sync
+[ $? -gt 0 ] && GRAND_EXIT=1
+
 [ "$(date '+%d')" == "01" ] && /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly
+[ $? -gt 0 ] && GRAND_EXIT=1
+
 [ "$(date '+%u')" == "1" ] && /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly
+[ $? -gt 0 ] && GRAND_EXIT=1
+
 /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh daily
+[ $? -gt 0 ] && GRAND_EXIT=1
+
 /opt/sysadmws/rsnapshot_backup/check_backup.sh 2
+[ $? -gt 0 ] && GRAND_EXIT=1
+
+exit $GRAND_EXIT
