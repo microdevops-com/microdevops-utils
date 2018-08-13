9fee0f9b27e7e33f2648ab6246d9618c0257ab81
Eugene Antonov <jetman@sysadm.ws>
2018-08-13 12:32:53 +0300

    rsnapshot_backup_sync_monthly_weekly_daily_check_backup more logging

--- rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh
+++ rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh
@@ -2,14 +2,28 @@
 
 GRAND_EXIT=0
 
+date '+%F %T ' | tr -d '\n'
+echo -e >&2 "NOTICE: Running /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh sync"
 /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh sync || GRAND_EXIT=1
 
-[ "$(date '+%d')" == "01" ] && ( /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly || GRAND_EXIT=1 )
+if [ "$(date '+%d')" == "01" ]; then
+	date '+%F %T ' | tr -d '\n'
+	echo -e >&2 "NOTICE: Running /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly"
+	/opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly || GRAND_EXIT=1
+fi
 
-[ "$(date '+%u')" == "1" ] && ( /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly || GRAND_EXIT=1 )
+if [ "$(date '+%u')" == "1" ]; then
+	date '+%F %T ' | tr -d '\n'
+	echo -e >&2 "NOTICE: Running /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly"
+	/opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly || GRAND_EXIT=1
+fi
 
+date '+%F %T ' | tr -d '\n'
+echo -e >&2 "NOTICE: Running /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh daily"
 /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh daily || GRAND_EXIT=1
 
+date '+%F %T ' | tr -d '\n'
+echo -e >&2 "NOTICE: Running /opt/sysadmws/rsnapshot_backup/check_backup.sh 2"
 /opt/sysadmws/rsnapshot_backup/check_backup.sh 2 || GRAND_EXIT=1
 
 exit $GRAND_EXIT
