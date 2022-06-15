85acb122c3f6569eed0a71390462329c81231ece
Eugene Antonov <jetman@sysadm.ws>
2022-06-15 14:26:46 +0300

    rsnapshot_backup.py lockfile req

--- misc/install_requirements.sh
+++ misc/install_requirements.sh
@@ -8,13 +8,13 @@ if [[ "${ID_LIKE}" = "debian" || "${ID}" = "debian" ]]; then
 	apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install gawk rsnapshot jq
 	# Python
 	if [[ "${UBUNTU_CODENAME}" = "focal" || "${UBUNTU_CODENAME}" = "jammy" || "${VERSION_CODENAME}" = "bullseye" ]]; then
-		apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install python3 python3-yaml python3-jinja2 python3-zmq
+		apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install python3 python3-yaml python3-jinja2 python3-zmq python3-lockfile
 		# Install python-is-python3 if python2 is not installed
 		if ! which python; then
 			apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install python-is-python3
 		fi
 	else
-		apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install python python-yaml python-jinja2 python-zmq
+		apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install python python-yaml python-jinja2 python-zmq python-lockfile
 	fi
 fi
 
@@ -23,9 +23,9 @@ if [[ "${ID_LIKE}" = "rhel fedora" ]]; then
 	yum install -y gawk rsnapshot jq
 	# Python
 	if [[ "${VERSION_ID}" = "7" ]]; then
-		yum install -y python2-pyyaml python2-zmq python2-jinja2
+		yum install -y python2-pyyaml python2-zmq python2-jinja2 python2-lockfile
 	elif [[ "${VERSION_ID}" = "8" ]]; then
-		yum install -y python3-pyyaml python3-zmq python3-jinja2
+		yum install -y python3-pyyaml python3-zmq python3-jinja2 python3-lockfile
 		if alternatives --display python | grep -q "python - status is auto"; then
 			alternatives --set python /usr/bin/python3
 			echo python set to /usr/bin/python3
