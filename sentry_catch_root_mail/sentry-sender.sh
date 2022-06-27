d8e36fae7b80e60a3de21a167d3601a5e9c1bedd
Алексей Кондратюк <alex.smesh@gmail.com>
2022-06-27 21:07:33 +0300

    Update sentry-sender.sh

--- sentry_catch_root_mail/sentry-sender.sh
+++ sentry_catch_root_mail/sentry-sender.sh
@@ -14,9 +14,6 @@ while IFS= read line; do
                 subject="-m '$val'"
                 continue;
         fi
-        if [[ "${key}" == "From" ]]; then
-                host="$(echo $val| sed -E 's/[^@]*@([^> ]*).*/\1/g')"
-        fi
         lines="${lines:-} -m '$line'"
 done
 
@@ -27,12 +24,11 @@ TMP_SENDER="$(mktemp /tmp/sender.XXXXXXXXX.sh)"
 cat << EOF > "${TMP_SENDER}"
 #!/bin/bash
 eval "\$(/usr/local/bin/sentry-cli bash-hook)"
-
 # LOG TEMP-SENDER MAIN COMMAND LINE
-echo "/usr/local/bin/sentry-cli send-event -t host:${host} ${subject} ${lines}" | sed 's/-m/\\\ \n -m/g;s/-t/\n -t/g' >> "${logfile}"
+echo "/usr/local/bin/sentry-cli send-event -t host:$(hostname -f) ${subject} ${lines}" | sed 's/-m/\\\ \n -m/g;s/-t/\n -t/g' >> "${logfile}"
 echo >> "${logfile}"
 
-/usr/local/bin/sentry-cli send-event -t host:${host} ${subject} ${lines} 2>&1 |tee -a "${logfile}"
+/usr/local/bin/sentry-cli send-event -t host:$(hostname -f) ${subject} ${lines} 2>&1 |tee -a "${logfile}"
 EOF
 
 # MAKE TEMP-SENDER RUNABLE
@@ -42,4 +38,4 @@ chmod +x "${TMP_SENDER}"
 "${TMP_SENDER}"
 
 # REMOVE TEMP-SENDER
-#rm -f "${TMP_SENDER}"
+rm -f "${TMP_SENDER}"
