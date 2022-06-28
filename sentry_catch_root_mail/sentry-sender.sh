ae3b4c2afa807a3877937bd548bb762531faccce
Алексей Кондратюк <alex.smesh@gmail.com>
2022-06-28 16:33:19 +0300

    Update sentry-sender.sh

--- sentry_catch_root_mail/sentry-sender.sh
+++ sentry_catch_root_mail/sentry-sender.sh
@@ -11,37 +11,30 @@ while IFS= read line; do
         key=$(echo "${line}" | sed -E 's/([^:]*).*/\1/g')
         val=$(echo "${line}" | sed -E 's/([^:]*): (.*)/\2/g')
         if [[ "${key}" == "Subject" ]]; then
-                subject="-m '$val'"
+                subject="-m '${val}'"
                 continue;
         fi
-        lines="${lines:-} -m '$line'"
+        lines="${lines:-} -m '${line}'"
 done
+
+# WIPE LOG FILE IF SIZE GREATER THAN 1M
+if [[ "$(wc -c < ${logfile})" -ge 1000000 ]]; then
+        echo > "${logfile}"
+fi
+# LOG MAIN COMMAND LINE
+echo "/usr/local/bin/sentry-cli send-event \ -t host:$(hostname -f) ${subject} ${lines}" | sed 's/-m/\\\ \n -m/g;s/-t/\n -t/g' >> "${logfile}"
+echo >> "${logfile}"
 
 # TEMP-SENDER PATH
 TMP_SENDER="$(mktemp /tmp/sender.XXXXXXXXX.sh)"
-
 # MAKE TEMP-SENDER
 cat << EOF > "${TMP_SENDER}"
 #!/bin/bash
-eval "\$(/usr/local/bin/sentry-cli bash-hook)"
-
-# WIPE LOG FILE IF SIZE GREATER THAN 1M
-if [[ \$(wc -c < ${logfile}) -ge 1000000 ]]; then
-        echo > "${logfile}"
-fi
-
-# LOG TEMP-SENDER MAIN COMMAND LINE
-echo "/usr/local/bin/sentry-cli send-event -t host:$(hostname -f) ${subject} ${lines}" | sed 's/-m/\\\ \n -m/g;s/-t/\n -t/g' >> "${logfile}"
-echo >> "${logfile}"
-
 /usr/local/bin/sentry-cli send-event -t host:$(hostname -f) ${subject} ${lines} 2>&1 |tee -a "${logfile}"
 EOF
-
 # MAKE TEMP-SENDER RUNABLE
 chmod +x "${TMP_SENDER}"
-
 # RUN TEMP-SENDER
 "${TMP_SENDER}"
-
 # REMOVE TEMP-SENDER
 rm -f "${TMP_SENDER}"
