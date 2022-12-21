6a3a9b307090e7d063e58638928e8b4ac6181d1b
Eugene Antonov <jetman@microdevops.com>
2022-12-21 15:41:44 +0200

    more filter for mysqlsh dump

--- rsnapshot_backup/rsnapshot_backup.py
+++ rsnapshot_backup/rsnapshot_backup.py
@@ -570,7 +570,7 @@ if __name__ == "__main__":
                                             databases_exclude = ""
 
                                         # Regex dots are to hide words that produce false positive
-                                        mysqlsh_output_filter = 'grep -v -e "The dump may f..l with an e...r if schema changes are made while dumping" -e "Writing DDL" -e "Data dump for"'
+                                        mysqlsh_output_filter = 'grep -v -e "The dump may f..l with an e...r if schema changes are made while dumping" -e "Writing DDL" -e "Data dump for" -e "Found e...rs loading plugins"'
 
                                         if item["source"] == "ALL":
                                             script_dump_part = textwrap.dedent(
