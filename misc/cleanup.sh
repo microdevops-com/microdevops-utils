01672b7a0846f7ad809e74c1b044904bf9b6cc1f
Eugene Antonov <jetman@microdevops.com>
2022-12-25 16:46:26 +0200

    cleanup fixes

---
+++ misc/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Cleanup leftover files from previous versions
+
+rm -rf /opt/sysadmws/sentry_catch_root_mail
+rm -f /opt/sysadmws/misc/install_requirements.sh
--- misc/tgz_cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# When installing via tgz we need to cleanup old utils manually
-
-rm -rf /opt/sysadmws/sentry_catch_root_mail
