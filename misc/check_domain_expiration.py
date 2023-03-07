26e846c3d0684b6a97def29fb6593bb7428ff206
Eugene Antonov <jetman@microdevops.com>
2023-03-07 11:12:53 +0200

    shebang forced python and typos

--- misc/check_domain_expiration.py
+++ misc/check_domain_expiration.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-                       
+
 import click
-import whois # pip3 install whois       
+import whois # pip3 install whois
 import datetime
 from pprint import pprint
 
@@ -10,29 +10,29 @@ from pprint import pprint
 @click.option("--days-warning", type=click.INT, default=28, help="Days to warning, default 28")
 @click.option("--days-critical", type=click.INT, default=7, help="Days to critical, default 7")
 def main(domain, days_warning, days_critical):
-                                      
+
     exit_code = 0
     try:
-        query = whois.query(domain)  
+        query = whois.query(domain)
         if query.expiration_date is None:
             exit_code = 2
         else:
             expiration_date = query.expiration_date
-            days = (expiration_date - datetime.datetime.now()).days                                                                                                                                            
-            if days < days_critical:                                                                                                                                                                           
-                print("CRITICAL: Domain %s expires in %d days" % (domain, days))                                                                                                                               
-                exit_code = 2                                                                                                                                                                                  
-            elif days < days_warning:                                                                                                                                                                          
-                print("WARNING: Domain %s expires in %d days" % (domain, days))                                                                                                                                
+            days = (expiration_date - datetime.datetime.now()).days
+            if days < days_critical:
+                print("CRITICAL: Domain %s expires in %d days" % (domain, days))
+                exit_code = 2
+            elif days < days_warning:
+                print("WARNING: Domain %s expires in %d days" % (domain, days))
                 exit_code = 1
-            else:                                                                                                                                                                                              
-                print("OK: Domain %s expires in %d days" % (domain, days))                                                                                                                                     
+            else:
+                print("OK: Domain %s expires in %d days" % (domain, days))
     except Exception as e:
-        print("CRITICAL: %s" % e)        
-        exit(2)                 
-    else:   
-        pprint(vars(query))        
-        exit(exit_code)     
+        print("CRITICAL: %s" % e)
+        exit(2)
+    else:
+        pprint(vars(query))
+        exit(exit_code)
 
 if __name__ == "__main__":
     main()
--- misc/shebang_python_switcher.sh
+++ misc/shebang_python_switcher.sh
@@ -3,7 +3,9 @@ if [[ -e /etc/os-release ]]; then
 	. /etc/os-release
 fi
 
-if [[ "${UBUNTU_CODENAME}" = "focal" || "${UBUNTU_CODENAME}" = "jammy" || ( "${ID_LIKE}" = "rhel fedora" && "${VERSION_ID}" = "8" ) || "${VERSION_CODENAME}" = "bullseye" ]]; then
+if [[ -f /opt/microdevops/misc/shebang_python_switcher.conf ]]; then
+	exec $(cat /opt/microdevops/misc/shebang_python_switcher.conf) "$@"
+elif [[ "${UBUNTU_CODENAME}" = "focal" || "${UBUNTU_CODENAME}" = "jammy" || ( "${ID_LIKE}" = "rhel fedora" && "${VERSION_ID}" = "8" ) || "${VERSION_CODENAME}" = "bullseye" ]]; then
 	exec /usr/bin/env python3 "$@"
 else
 	exec /usr/bin/env python "$@"
