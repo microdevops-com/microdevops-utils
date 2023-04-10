a9f1df89799b4fb18a252f6ef3f9a90b6efaab44
Eugene Antonov <jetman@microdevops.com>
2023-04-10 16:54:46 +0300

    pxc_check wsrep status 2 is also ok

--- misc/pxc_check.sh
+++ misc/pxc_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 STATUS=$(echo "show status like 'wsrep_local_state'" | mysql | tail -n 1 | awk '{print $2}')
-if [[ ${STATUS} = 4 ]]; then
+# https://docs.percona.com/percona-xtradb-cluster/8.0/wsrep-status-index.html#wsrep_local_state_comment
+if [[ ${STATUS} == 4 || ${STATUS} == 2 ]]; then
 	exit 0
 else
 	exit 1
