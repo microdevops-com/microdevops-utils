25dd6b477a4473573c64a7008eff10fc6ee32fd8
Eugene Antonov <jetman@microdevops.com>
2023-04-10 16:50:44 +0300

    filter out kubernetes.io volumes

--- disk_alert/disk_alert.sh
+++ disk_alert/disk_alert.sh
@@ -35,7 +35,7 @@ fi
 if [[ _$DISK_ALERT_FILTER != "_" ]]; then
 	FILTER=$DISK_ALERT_FILTER
 else
-	FILTER="^Filesystem|^tmpfs|^cdrom|^none|^/dev/loop|^overlay|^shm|^udev|^cgroup|^cgmfs|^snapfuse"
+	FILTER="^Filesystem|^tmpfs|^cdrom|^none|^/dev/loop|^overlay|^shm|^udev|^cgroup|^cgmfs|^snapfuse|^kubernetes.io"
 fi
 #
 if [[ _$DISK_ALERT_USAGE_CHECK == "_PERCENT" ]]; then
