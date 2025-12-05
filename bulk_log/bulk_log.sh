6e52de311eff8d5edf6b9617d08a946195e74036
vas-mdvps <vas@microdevops.com>
2025-12-05 08:53:50 +0100

    Update bulk_log.sh
    an empty line divider between blocks

--- bulk_log/bulk_log.sh
+++ bulk_log/bulk_log.sh
@@ -4,36 +4,52 @@ GW_IP_ADDRESS=$(/sbin/ip route | awk '/default/ { print $3 }')
 
 echo "#########################################################################################################"
 date
+echo " "
 echo "### top"
 COLUMNS=250 top -b -n 1 -c 2>&1
+echo " "
 echo "### ps"
 ps aux 2>&1
+echo " "
 echo "### iotop"
 iotop -b -k -t -n 1 2>&1
+echo " "
 echo "### free"
 free -m 2>&1
+echo " "
 echo "### uptime"
 uptime
+echo " "
 echo "### ping"
 ping -c 10 www.google.com 2>&1
+echo " "
 echo "### netstat"
 netstat -an 2>&1
+echo " "
 echo "### ping gw"
 ping -c 4 ${GW_IP_ADDRESS} 2>&1
+echo " "
 echo "### arp"
 arp -an 2>&1
+echo " "
 echo "### ifconfig"
 ifconfig -a 2>&1
+echo " "
 echo "### ethtool"
 ethtool eth0 2>&1
+echo " "
 echo "### ethtool"
 ethtool -S eth0 2>&1
+echo " "
 echo "### netstat"
 netstat -ia 2>&1
+echo " "
 echo "### netstat -nr"
 netstat -nr 2>&1
+echo " "
 echo "### w"
 w 2>&1
+echo " "
 echo "### df"
 df -h 2>&1
 echo " "
